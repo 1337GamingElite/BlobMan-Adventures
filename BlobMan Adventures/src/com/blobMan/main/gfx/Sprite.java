eb3ffa1d99ea9f4dcd9738ef6ab73d0a74aee473
1337GamingElite <1337GamingElite@gmail.com>
2016-06-11 21:55:34 -0600

    Changed the void sprite from light blue to black

--- BlobMan Adventures/src/com/blobMan/main/gfx/Sprite.java
+++ BlobMan Adventures/src/com/blobMan/main/gfx/Sprite.java
@@ -13,7 +13,7 @@ public class Sprite {
 	public static Sprite rock = new Sprite(16, 2, 0, SpriteSheet.tiles);
 	public static Sprite dirt = new Sprite(16, 3, 0, SpriteSheet.tiles);
 	
-	public static Sprite voidSprite = new Sprite(16, 0x00FBFF);
+	public static Sprite voidSprite = new Sprite(16, 0x000000);
 
 	public static Sprite player_up = new Sprite(32, 2, 6, SpriteSheet.tiles);
 	public static Sprite player_down = new Sprite(32, 0, 6, SpriteSheet.tiles);
