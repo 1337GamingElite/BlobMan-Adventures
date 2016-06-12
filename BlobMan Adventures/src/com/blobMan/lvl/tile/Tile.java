3d807c4cc367479f567ffcf6b5b896ff119408e1
1337GamingElite <1337GamingElite@gmail.com>
2016-06-12 13:37:15 -0600

    Added: Bushes, Water, Stone Wall, and WoodFloor

--- BlobMan Adventures/src/com/blobMan/lvl/tile/Tile.java
+++ BlobMan Adventures/src/com/blobMan/lvl/tile/Tile.java
@@ -2,9 +2,13 @@ package com.blobMan.lvl.tile;
 
 import com.blobMan.lvl.tile.flowers.BlueFlowerTile;
 import com.blobMan.lvl.tile.flowers.YellowFlowerTile;
-import com.blobMan.lvl.tile.general_tiles.DirtTile;
-import com.blobMan.lvl.tile.general_tiles.GrassTile;
-import com.blobMan.lvl.tile.general_tiles.RockTile;
+import com.blobMan.lvl.tile.general_tiles.man_made.StoneWallTile;
+import com.blobMan.lvl.tile.general_tiles.man_made.WoodFloorTile;
+import com.blobMan.lvl.tile.general_tiles.nature.BushTile;
+import com.blobMan.lvl.tile.general_tiles.nature.DirtTile;
+import com.blobMan.lvl.tile.general_tiles.nature.GrassTile;
+import com.blobMan.lvl.tile.general_tiles.nature.RockTile;
+import com.blobMan.lvl.tile.general_tiles.nature.WaterTile;
 import com.blobMan.main.gfx.Screen;
 import com.blobMan.main.gfx.Sprite;
 
@@ -13,12 +17,23 @@ public class Tile {
 	public int x, y;
 	public Sprite sprite;
 	
+	public static Tile voidTile = new VoidTile(Sprite.voidSprite);
+	
+	// General Tiles
+	// Nature
 	public static Tile grass = new GrassTile(Sprite.grass);
-	public static Tile yellowFlower = new YellowFlowerTile(Sprite.yellowFlower);
-	public static Tile blueFlower = new BlueFlowerTile(Sprite.blueFlower);
 	public static Tile rock = new RockTile(Sprite.rock);
 	public static Tile dirt = new DirtTile(Sprite.dirt);
-	public static Tile voidTile = new VoidTile(Sprite.voidSprite);
+	public static Tile water = new WaterTile(Sprite.water);
+	public static Tile bush = new BushTile(Sprite.bush);
+	
+	// Man-Made
+	public static Tile woodFloor = new WoodFloorTile(Sprite.woodFloor);
+	public static Tile stoneWall = new StoneWallTile(Sprite.stoneWall);
+	
+	// Flowers
+	public static Tile yellowFlower = new YellowFlowerTile(Sprite.yellowFlower);
+	public static Tile blueFlower = new BlueFlowerTile(Sprite.blueFlower);
 
 	public Tile(Sprite sprite) { 
 		this.sprite = sprite;
